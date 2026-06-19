57a82cb4fe118e070d9a08ce437b19c0e22c8084
wilku <29576048+wilku103@users.noreply.github.com>
2026-06-19 21:01:14 +0200

    Add statistics
    Added statistics for frequency of rolls, running average, high/low and
    parity

--- ui/statistics_page.py
+++ ui/statistics_page.py
@@ -1,6 +1,15 @@
+import numpy as np
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-from PyQt6.QtWidgets import QPushButton, QVBoxLayout, QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QSizePolicy,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 class Ui_StatisticsPage(QWidget):
@@ -14,24 +23,113 @@ class Ui_StatisticsPage(QWidget):
         self.figure = Figure(figsize=(5, 4), dpi=100)
         self.canvas = FigureCanvas(self.figure)
 
+        self.label = QLabel(self)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setSizePolicy(
+            QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Maximum
+        )
+
+        self.main_layout.addWidget(self.label)
         self.main_layout.addWidget(self.canvas)
-        self.ax = self.figure.add_subplot(111)
+
+        self.axes = {}
+        self.axes["freq"] = self.figure.add_subplot(221)
+        self.axes["avg"] = self.figure.add_subplot(222)
+        self.axes["luck"] = self.figure.add_subplot(223)
+        self.axes["parity"] = self.figure.add_subplot(224)
 
         self.rolls_btn = QPushButton("Rolling", self)
+        self.reset_btn = QPushButton("Reset rolls", self)
+
+        self.interface_box = QHBoxLayout()
+        self.interface_box.addWidget(self.reset_btn)
+        self.interface_box.addWidget(self.rolls_btn)
+        self.main_layout.addLayout(self.interface_box)
+
+    def update_freq_ax(self):
+        ax = self.axes["freq"]
+
+        counts, _ = np.histogram(self.rolls, bins=np.arange(1, 8) - 0.5)
+        frequencies = counts / len(self.rolls)
+
+        ax.bar(
+            range(1, 7),
+            frequencies,
+            label="Zaobserwowane",
+        )
+        ax.axhline(1 / 6, color="red", linestyle="--", label="Teoretyczne")
+        ax.set_title("Częstotliwości")
+        ax.set_xlabel("Wartość")
+        ax.set_ylabel("Częstotliwość")
+        ax.set_xticks(range(1, 7))
+        ax.legend()
+
+    def update_average_ax(self):
+        ax = self.axes["avg"]
+        avg = np.cumsum(self.rolls) / np.arange(1, len(self.rolls) + 1)
+
+        ax.plot(
+            np.arange(1, len(self.rolls) + 1),
+            avg,
+            label="Średnia",
+        )
+        ax.axhline(
+            7 / 2, color="red", linestyle="--", label=f"Wartość Oczekiwana: {7 / 2}"
+        )
+
+        ax.set_xlabel("Liczba rzutów")
+        ax.set_title("Średnia")
+        ax.set_ylabel("Wartość")
+        ax.legend()
+
+    def update_luck_ax(self):
+        ax = self.axes["luck"]
+        luck = {
+            "high": len([x for x in self.rolls if x >= 4]) / len(self.rolls),
+            "low": len([x for x in self.rolls if x <= 3]) / len(self.rolls),
+        }
+
+        ax.bar(
+            ["Wysokie", "Niskie"],
+            luck.values(),
+            label="Zaobserwowane",
+        )
+        ax.axhline(0.5, color="red", linestyle="--", label="Teoretyczne")
+
+        ax.set_title("Częstotliwość wysokich i niskich rzutów")
+        ax.set_ylabel("Częstotliwość")
+        ax.legend()
+
+    def update_parity_ax(self):
+        ax = self.axes["parity"]
+
+        parity = {
+            "even": len([x for x in self.rolls if x % 2 == 1]) / len(self.rolls),
+            "odd": len([x for x in self.rolls if x % 2 == 0]) / len(self.rolls),
+        }
+
+        ax.bar(
+            ["Parzyste", "Nieparzyste"],
+            parity.values(),
+            label="Zaobserwowane",
+        )
+        ax.axhline(0.5, color="red", linestyle="--", label="Teoretyczne")
+
+        ax.set_title("Częstotliwość parzystych i nieparzystych rzutów")
+        ax.set_ylabel("Częstotliwość")
+        ax.legend()
 
     def showEvent(self, _):
-        self.ax.clear()
-        max_value = max(list(self.rolls)) if self.rolls else 0
-        counts_total = [0] * max_value
-        for sides, rolls in self.rolls.items():
-            for roll in rolls:
-                counts_total[roll - 1] += 1
+        for ax in self.axes.values():
+            ax.clear()
 
-        print(counts_total)
-        self.ax.bar(
-            range(1, max_value + 1),
-            counts_total,
-            label=f"Total rolls: {sum(counts_total)}",
-        )
-        self.ax.legend()
+        self.update_freq_ax()
+        self.update_average_ax()
+        self.update_luck_ax()
+        self.update_parity_ax()
+
+        self.figure.tight_layout()
+
+        self.label.setText(f"Na podstawie {len(self.rolls)} rzutów")
+
         self.canvas.draw()
